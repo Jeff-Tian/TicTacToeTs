4e71b7398d6bc7af81179c90fe8419ff754fd55a
Jeff Tian <jeff.tian@outlook.com>
2021-12-26 11:18:47 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './App.css'
+import {Game} from './Game'
+
+export default function App() {
+  return (
+    <main>
+      React⚛️ + Vite⚡ + Replit🌀
+      <Game />
+    </main>
+  )
+}
