a638d4e9e37d57792236ef3b131709064d7e1f78
Jeff Tian@Workstation <jie.tian@live.cn>
2022-01-20 15:38:31 +0800

    feat: copy paste tic tac toe ai

--- src/GameAI.tsx
+++ src/GameAI.tsx
@@ -170,3 +170,409 @@ function calculateWinner(squares) {
     }
     return null;
 }
+
+let latestFactors = null;
+
+class AI {
+    constructor() {
+        this.weights = Object.assign([], Strategy.getInitialWeights());
+        this.setWeightsUpdatedCallback(function () {
+        });
+    }
+
+    static nextMove(squares, weights) {
+        spotScoreMap.clear();
+        let spots = Judger.getSpots(squares);
+        let nextBoards = Judger.generateNewBoardsBySpots(squares, spots);
+        let scores = nextBoards.map(b => Judger.getBoardScore(b, weights).total);
+
+        for (let i = 0; i < spots.length; i++) {
+            const spot = spots[i];
+            const score = scores[i];
+
+            spotScoreMap.set(spot, {
+                weights: weights,
+                strategy: Strategy.getBoardStatus(squares).factors,
+                score: score,
+            });
+        }
+
+        let index = ArrayHelper.findIndexOfMax(scores);
+        latestFactors = scores[index].namedFactors;
+
+        return spots[index];
+    }
+
+    setWeightsUpdatedCallback(cb) {
+        this.weightsUpdatedCallback = cb;
+    }
+
+    nextMove(squares) {
+        this.tryLearn(squares);
+        return AI.nextMove(squares, this.weights);
+    }
+
+    tryLearn(squares) {
+        this.learn(this.lastBitmapSquares, squares);
+        this.lastBitmapSquares = squares;
+    }
+
+    clean() {
+        latestFactors = null;
+    }
+
+    getWeights() {
+        return this.weights;
+    }
+
+    getFactors() {
+        return latestFactors;
+    }
+
+    setWeights(weights) {
+        this.weights = weights;
+    }
+
+    learn(lastSquares, currentSquares) {
+        if (!lastSquares) {
+            return;
+        }
+
+        if (!GlobalSettings.learn) {
+            return;
+        }
+
+        let estimatedLastScore = Judger.getBoardScore(lastSquares, this.weights);
+        let actualScore = Judger.getBoardScore(currentSquares, this.weights);
+        let diff = actualScore.total - estimatedLastScore.total;
+
+        for (let i = 0; i < estimatedLastScore.factors.length; i++) {
+            this.weights[i] = this.weights[i] + 0.1 * diff * estimatedLastScore.factors[i];
+        }
+
+        this.weightsUpdatedCallback(this.weights);
+    }
+}
+
+const boardSides = {
+    top: [0, 1, 2],
+    left: [0, 3, 6],
+    right: [2, 5, 8],
+    bottom: [6, 7, 8],
+    center: [3, 4, 5],
+    middle: [1, 4, 7],
+    slash: [2, 4, 6],
+    antiSlash: [0, 4, 8]
+};
+
+const sides = [
+    boardSides.top,
+    boardSides.center,
+    boardSides.bottom,
+    boardSides.left,
+    boardSides.middle,
+    boardSides.right,
+    boardSides.slash,
+    boardSides.antiSlash
+];
+
+function checkSides(bitmap) {
+    let d = 0;
+    let dead = 0;
+    let w = 0;
+    let c = 0;
+
+    for (let i = 0; i < sides.length; i++) {
+        let side = bitmap.filter((_, j) => sides[i].indexOf(j) >= 0);
+
+        let negatives = side.filter(b => b === -1);
+        let zeros = side.filter(b => b === 0);
+        let ones = side.filter(b => b === 1);
+
+        if (negatives.length === 2 && zeros.length === 1) {
+            d++;
+        }
+
+        if (negatives.length === 3) {
+            dead++;
+        }
+
+        if (ones.length === 3) {
+            w++;
+        }
+
+        if (ones.length === 2 && zeros.length === 1) {
+            c++;
+        }
+    }
+
+    return {danger: d, lost: dead, chance: c, win: w};
+}
+
+let initialWeights = [0, 1, 1];
+let namedStrategy = (factors) => {
+    return {
+        const: factors[0],
+        danger: factors[1],
+        occupyCenter: factors[2],
+    };
+}
+
+export class StrategySettings {
+    static setInitialWeights(iw) {
+        initialWeights = iw;
+    }
+
+    static setNamedStrategy(func) {
+        namedStrategy = func;
+    }
+}
+
+export default class Strategy {
+    static getInitialWeights() {
+        return initialWeights;
+    }
+
+    static getNamedStrategy(factors) {
+        return namedStrategy(factors);
+    }
+
+    static getBoardStatus(bitmap) {
+        let {danger, lost, chance, win} = checkSides(bitmap);
+        return {
+            danger, lost, chance, win,
+            factors: Object.keys(namedStrategy(Strategy.getInitialWeights())).map(key => {
+                return {
+                    const: 1,
+                    danger: danger * 1.1,
+                    occupyCenter: bitmap[4] === 1 ? 1 : 0,
+                    intersectedBads: Strategy.getIntersectedBads(bitmap) / 2,
+                    chance: chance,
+                    numberOfBadsOfMyChance: Strategy.getNumberOfBadsOfMyChancePosition(bitmap)
+                }[key];
+            })
+        };
+    }
+
+    static getIntersectedBads(bitmap) {
+        let intersectedBads = 0;
+        const bads = [];
+
+        for (let i = 0; i < sides.length; i++) {
+            let side = bitmap.filter((_, j) => sides[i].indexOf(j) >= 0);
+
+            const [v1, v2, v3] = side;
+
+            if ((v1 === -1 && v2 === 0 && v3 === 0) ||
+                (v1 === 0 && v2 === -1 && v3 === 0) ||
+                (v1 === 0 && v2 === 0 && v3 === -1) ||
+
+                (v1 === -1 && v2 === -1 && v3 === 0) ||
+                (v1 === -1 && v2 === 0 && v3 === -1) ||
+                (v1 === 0 && v2 === -1 && v3 === -1)
+            ) {
+                bads.push(sides[i]);
+            }
+        }
+
+        if (bads.length <= 1) {
+            return 0;
+        }
+
+        for (let i = 0; i < bads.length - 1; i++) {
+            for (let j = i + 1; j < bads.length; j++) {
+                const bad1 = bads[i];
+                const bad2 = bads[j];
+
+                const intersects = ArrayHelper.intersects(bad1, bad2);
+
+                if (intersects.length > 0 && (bitmap[intersects[0]] === 0 || bitmap[intersects[0]] === -1)) {
+                    intersectedBads++;
+                }
+            }
+        }
+
+        return intersectedBads;
+    }
+
+    static getNumberOfBadsOfMyChancePosition(bitmap) {
+        let sum = 0;
+
+        for (let i = 0; i < sides.length; i++) {
+            let sideValues = bitmap.filter((_, j) => sides[i].indexOf(j) >= 0);
+
+            let zeros = sideValues.filter(b => b === 0);
+            let ones = sideValues.filter(b => b === 1);
+
+            if (ones.length === 2 && zeros.length === 1) {
+                const theChance = sides[i].filter(index => bitmap[index] === 0)[0];
+
+                if (theChance !== undefined) {
+                    let sidesContainsTheChancePosition = sides.filter(s => s.indexOf(theChance) >= 0);
+
+                    for (const s of sidesContainsTheChancePosition) {
+                        let sValues = bitmap.filter((_, j) => s.indexOf(j) >= 0);
+
+                        let n = sValues.filter(b => b === -1).length;
+                        let p = sValues.filter(b => b === 1).length;
+
+                        if (n > 0 && p === 0) {
+                            sum++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return sum;
+    }
+}
+
+const ArrayHelper = {
+    convertIndexToRowColumn(rowLength, index) {
+        return {
+            col: (index % rowLength) + 1,
+            row: Math.floor(index / rowLength) + 1
+        }
+    },
+    is2DArray: function (array) {
+        return array[0] instanceof Array;
+    },
+    getRowColumnByIndex(array, index) {
+        if (this.is2DArray(array)) {
+            return this.convertIndexToRowColumn(array[0].length, index);
+        }
+
+        return this.convertIndexToRowColumn(Math.sqrt(array.length), index);
+    },
+
+    findIndexOfMax(array) {
+        let index = 0;
+        let max = array[0];
+
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                index = i;
+            }
+        }
+
+        return index;
+    },
+
+    intersects(a1, a2) {
+        const res = [];
+        for (const e1 of a1) {
+            if (a2.indexOf(e1) >= 0) {
+                res.push(e1);
+            }
+        }
+
+        return res;
+    }
+}
+
+export const spotScoreMap = new Map();
+
+let defaultSettings = {
+    // learn: true,
+    // showLearningStatus: false,
+    language: ['en-US']
+};
+export const GlobalSettings = {
+    ...defaultSettings
+};
+
+const Judger = {
+    getBoardScore: function (bitmap, weights) {
+        weights = weights || Strategy.getInitialWeights();
+
+        let {lost, win, factors} = Strategy.getBoardStatus(bitmap);
+
+        if (lost) {
+            return {
+                factors: factors,
+                namedFactors: Strategy.getNamedStrategy(factors),
+                total: -Math.PI / 2
+            }
+        }
+
+        if (win) {
+            return {
+                factors: factors,
+                namedFactors: Strategy.getNamedStrategy(factors),
+                total: Math.PI / 2
+            }
+        }
+
+        let score = Math.atan(factors.map((s, i) => s * weights[i]).reduce((prev, next) => prev + next, 0));
+
+        return {
+            factors: factors,
+            namedFactors: Strategy.getNamedStrategy(factors),
+            total: score
+        };
+    },
+
+    getSpots(bitmapSquares) {
+        return bitmapSquares.map((s, i) => s === 0 ? i : NaN).filter(n => !isNaN(n));
+    },
+
+    generateNewBoardsBySpots(currentBoard, spots) {
+        spots = spots || this.getSpots(currentBoard);
+
+        return spots.map(i => {
+            let newSquares = currentBoard.slice();
+            newSquares[i] = 1;
+
+            return newSquares;
+        });
+    },
+
+    gameProgress(bitmapSquares) {
+        let emptySpots = bitmapSquares.filter(b => b === 0);
+
+        if (emptySpots.length === bitmapSquares.length) {
+            return {
+                win: false,
+                lost: false,
+                fair: false
+            }
+        }
+
+        for (let i = 0; i < sides.length; i++) {
+            const theSide = sides[i];
+
+            let side = bitmapSquares.filter((_, j) => theSide.indexOf(j) >= 0);
+
+            let ones = side.filter(b => b === 1);
+            let negatives = side.filter(b => b === -1);
+
+            if (ones.length === 3) {
+                return {
+                    win: theSide,
+                    lost: false,
+                    fair: false
+                };
+            }
+
+            if (negatives.length === 3) {
+                return {
+                    win: false,
+                    lost: theSide,
+                    fair: false
+                };
+            }
+        }
+
+        return {
+            win: false,
+            lost: false,
+            fair: emptySpots.length === 0
+        }
+    },
+
+    gameEnds(progress) {
+        return progress.fair || progress.win || progress.lost;
+    }
+}
